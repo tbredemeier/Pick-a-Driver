ac7340cbab64447dba45f8833e7e7d3ebbc908d2
Tom Bredemeier <tbredemeier@barrington220.org>
2018-02-11 16:28:51 -0600

    completed Table View name editing features”)

--- Pick a Driver/NamesTableViewController.swift
+++ Pick a Driver/NamesTableViewController.swift
@@ -14,6 +14,7 @@ class NamesTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.isEditing = true
     }
 
     override func didReceiveMemoryWarning() {
@@ -36,40 +37,25 @@ class NamesTableViewController: UITableViewController {
         return cell
     }
 
-    /*
-     // Override to support conditional editing of the table view.
-     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the specified item to be editable.
-     return true
-     }
-     */
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
 
-    /*
-     // Override to support editing the table view.
-     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-     if editingStyle == .delete {
-     // Delete the row from the data source
-     tableView.deleteRows(at: [indexPath], with: .fade)
-     } else if editingStyle == .insert {
-     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-     }
-     }
-     */
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            self.names.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
 
-    /*
-     // Override to support rearranging the table view.
-     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+        let nameToMove = names.remove(at: fromIndexPath.row)
+        names.insert(nameToMove, at: to.row)
+    }
 
-     }
-     */
-
-    /*
-     // Override to support conditional rearranging of the table view.
-     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the item to be re-orderable.
-     return true
-     }
-     */
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
 
     @IBAction func onPlusButtonTapped(_ sender: UIBarButtonItem) {
         let alert = UIAlertController(title: "Add a name", message: nil,
@@ -80,7 +66,7 @@ class NamesTableViewController: UITableViewController {
         let cancelAction = UIAlertAction(title: "Cancel",
                                          style: .cancel, handler: nil)
         alert.addAction(cancelAction)
-        let appendAction = UIAlertAction(title: "Add",
+        let appendAction = UIAlertAction(title: "OK",
                                          style: .default) { (action) in
                                             let nameTextField = alert.textFields![0] as UITextField
                                             let name = nameTextField.text!
@@ -90,15 +76,4 @@ class NamesTableViewController: UITableViewController {
         alert.addAction(appendAction)
         present(alert, animated: true, completion: nil)
     }
-    
-    /*
-     // MARK: - Navigation
-
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destinationViewController.
-     // Pass the selected object to the new view controller.
-     }
-     */
-
 }
